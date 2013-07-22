dd6de49fa90fcf872865c0699853d357cb0a6e56
Blake Thomas <bwthomas@gmail.com>
2013-07-21 22:05:17 -0500

    Added tests for getters & setters.

--- README.md
+++ README.md
@@ -10,9 +10,9 @@ When you're done, please zip up this directory and send it back to us. Thanks!
 1.  ~~Make any changes necessary to be able to run the tests by running
     `ruby tree_node_test.rb` at the command line. Fix any failing tests.~~
 
-2.  Another developer has partially implemented TreeNode#name and
+2.  ~~Another developer has partially implemented TreeNode#name and
     TreeNode#name=, but failed to write any tests for that functionality. Add
-    some test coverage to those methods.
+    some test coverage to those methods.~~
 
 3.  Add a predicate instance method to TreeNode that takes a single TreeNode
     instance as an argument. The method should return whether the supplied
--- tree_node_test.rb
+++ tree_node_test.rb
@@ -1,21 +1,69 @@
+require 'simplecov'
+SimpleCov.start
+
 $:.unshift File.join(File.dirname(__FILE__))
 
 require 'test/unit'
 require 'tree_node'
 
-class TreeNodeTest < Test::Unit::TestCase
+class TreeNodeInitializationTest < Test::Unit::TestCase
 
   def setup
     @tree_node = TreeNode.new
   end
 
+  def test_accepts_and_sets_name_when_provided
+    assert_equal( 'lorem', TreeNode.new('lorem').name,
+                  'should use parameter for name when created' )
+  end
+
+  def test_has_no_name_by_default_when_created
+    assert_nil(@tree_node.name)
+  end
+
   def test_has_no_children_when_created
-    assert_equal(0, @tree_node.children_count,
-                 "should have an empty children array when created")
+    assert_equal( 0, @tree_node.children_count,
+                  'should have an empty children array when created' )
   end
 
   def test_has_no_parent_when_created
     assert_nil(@tree_node.parent)
+  end
+
+end
+
+class TreeNodeAttrAccessorTest < Test::Unit::TestCase
+
+  def setup
+    @tree_node = TreeNode.new
+  end
+
+  def test_setting_name
+    assert_nil(@tree_node.name)
+
+    @tree_node.name = 'ipsum'
+    assert_equal( 'ipsum', @tree_node.name,
+                  'should set name' )
+  end
+
+  def test_setting_parent
+    assert_nil(@tree_node.parent)
+
+    parent            = TreeNode.new('mommy dearest')
+    @tree_node.parent = parent
+    assert_equal( parent, @tree_node.parent,
+                  'should set parent' )
+  end
+
+  def test_setting_children
+    assert_empty(@tree_node.children)
+
+    children            = ['Donny', 'Marie']
+    @tree_node.children = children
+    assert_equal( children, @tree_node.children,
+                  'should set children' )
+    assert_equal( 2, @tree_node.children_count,
+                  'should have two children when assigned to array with two elements' )
   end
 
 end
@@ -29,8 +77,8 @@ class TreeNodeChildAssignmentTest < Test::Unit::TestCase
   end
 
   def test_children_count
-    assert_equal(1, @tree_node.children_count,
-                 "should have a single child when one is added")
+    assert_equal( 1, @tree_node.children_count,
+                  'should have a single child when one is added' )
   end
 
   def test_parent_assignment
